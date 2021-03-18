d8cdc4b3234d5f517a31bfdb6e9f163a7ef3b8fb
szatjp <szxatjp@163.com>
2021-03-18 15:37:09 +0800

    change

--- backup/wordedit.pas
+++ backup/wordedit.pas
@@ -17,7 +17,6 @@ type
     BitBtn2: TBitBtn;
     BitBtn3: TBitBtn;
     BitBtn4: TBitBtn;
-    BitBtn5: TBitBtn;
     BitBtn6: TBitBtn;
     BitBtn7: TBitBtn;
     BitBtn8: TBitBtn;
@@ -27,7 +26,7 @@ type
     DBEdit1: TDBEdit;
     DBEdit2: TDBEdit;
     DBEdit3: TDBEdit;
-    DBGrid1: TDBGrid;
+    grddyz: TDBGrid;
     DBGrid2: TDBGrid;
     Edit1: TEdit;
     GroupBox1: TGroupBox;
@@ -42,10 +41,36 @@ type
     SQLQuery1: TSQLQuery;
     SQLQuery2: TSQLQuery;
     ToolBar1: TToolBar;
+    ToolBar2: TToolBar;
+    ToolBar3: TToolBar;
     ToolButton1: TToolButton;
+    ToolButton10: TToolButton;
+    ToolButton11: TToolButton;
+    ToolButton12: TToolButton;
+    ToolButton13: TToolButton;
+    ToolButton14: TToolButton;
+    ToolButton15: TToolButton;
+    ToolButton16: TToolButton;
+    ToolButton17: TToolButton;
+    ToolButton18: TToolButton;
+    ToolButton19: TToolButton;
     ToolButton2: TToolButton;
+    ToolButton20: TToolButton;
+    ToolButton21: TToolButton;
+    ToolButton22: TToolButton;
+    ToolButton23: TToolButton;
+    ToolButton24: TToolButton;
+    ToolButton25: TToolButton;
+    ToolButton26: TToolButton;
+    ToolButton27: TToolButton;
+    ToolButton28: TToolButton;
     ToolButton3: TToolButton;
     ToolButton4: TToolButton;
+    ToolButton5: TToolButton;
+    ToolButton6: TToolButton;
+    ToolButton7: TToolButton;
+    ToolButton8: TToolButton;
+    ToolButton9: TToolButton;
     procedure BitBtn1Click(Sender: TObject);
     procedure BitBtn3Click(Sender: TObject);
     procedure BitBtn4Click(Sender: TObject);
@@ -54,6 +79,11 @@ type
     procedure FormCreate(Sender: TObject);
     procedure FormShow(Sender: TObject);
     procedure Panel1Click(Sender: TObject);
+    procedure SQLQuery1AfterScroll(DataSet: TDataSet);
+    procedure ToolBar1Click(Sender: TObject);
+    procedure ToolButton1Click(Sender: TObject);
+    procedure ToolButton2Click(Sender: TObject);
+    procedure ToolButton3Click(Sender: TObject);
   private
 
   public
@@ -74,6 +104,38 @@ uses main;
 procedure Tfrmdyzedit.Panel1Click(Sender: TObject);
 begin
 
+end;
+
+procedure Tfrmdyzedit.SQLQuery1AfterScroll(DataSet: TDataSet);
+begin
+  //SQLQuery1.Open;
+  SQLQuery2.SQL.Text := 'select from tbl_pinyin where fduoyinzi_id=:duoyinzi_id';
+  //SQLQuery1.InsertSQL.Text := 'INSERT INTO tbl_duoyinzi (fduoyinzi) VALUES (:duoyinzi)';
+  SQLQuery2.Params.ParamByName('duoyinzi_id').AsString := SQLQuery1.FieldValues['fid'];
+  //SQLQuery1.ExecSQL();
+  SQLQuery2.Open;
+end;
+
+procedure Tfrmdyzedit.ToolBar1Click(Sender: TObject);
+begin
+  //TToolButton(Sender).Caption;
+end;
+
+procedure Tfrmdyzedit.ToolButton1Click(Sender: TObject);
+begin
+  //DBEdit2.text := TToolButton(Sender).Caption;
+  //SelText 当前位置插入字符
+  DBEdit2.SelText:=TToolButton(Sender).Caption;
+end;
+
+procedure Tfrmdyzedit.ToolButton2Click(Sender: TObject);
+begin
+  DBEdit2.SelText:=TToolButton(Sender).Caption;
+end;
+
+procedure Tfrmdyzedit.ToolButton3Click(Sender: TObject);
+begin
+  DBEdit2.SelText:=TToolButton(Sender).Caption;
 end;
 
 procedure Tfrmdyzedit.BitBtn1Click(Sender: TObject);
@@ -112,14 +174,17 @@ end;
 
 procedure Tfrmdyzedit.FormCreate(Sender: TObject);
 begin
-  Edit1.SelText:='插入字符串';
+
 end;
 
 procedure Tfrmdyzedit.FormShow(Sender: TObject);
 begin
   //SQLQuery1.SQL.Text := 'select fduoyinzi from tbl_duoyinzi;';
+  //SQLQuery1.Active:=True;
   DBEdit1.DataField:='fduoyinzi';
   SQLQuery1.Active:=True;
+  grddyz.Columns[0].FieldName:='fid';
+  grddyz.Columns[1].FieldName:='fduoyinzi';
 end;
 
 end.
